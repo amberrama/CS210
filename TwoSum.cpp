1fc556decb9d7f774e4e2c11c3fbb567d4ea9bb0
arama5272 <arama5272@sdsu.edu>
2026-09-06 15:08:01 -0700

    Added Hash Table function

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,25 +1,71 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <utility>
 
-std::vector<int> twoSumBruteForce(const std::vector<int>& nums, int target) {
+std::pair<int, int> twoSumBruteForce(
+    const std::vector<int>& nums,
+    int target
+) {
     for (int i = 0; i < nums.size(); i++) {
         for (int j = i + 1; j < nums.size(); j++) {
-            if (nums[i] + nums [j] == target) {
+            if (nums[i] + nums[j] == target) {
                 return {i, j};
             }
         }
     }
-    return {};
+
+    return {-1, -1};
+}
+
+std::pair<int, int> twoSumHash(
+    const std::vector<int>& nums,
+    int target
+) {
+    std::unordered_map<int, int> seen;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int needed = target - nums[i];
+
+        if (seen.find(needed) != seen.end()) {
+            return {seen[needed], i};
+        }
+
+        seen[nums[i]] = i;
+    }
+
+    return {-1, -1};
 }
 
 int main() {
-    std::vector<int> nums = {15, 4, 18, 8, 19, 22, 24, 59, 59, 20, 18, 12, 36, 42, 9};
+    std::vector<int> nums = {
+        15, 4, 18, 8, 19, 22, 24, 59,
+        59, 20, 18, 12, 36, 42, 9
+    };
+
     int target = 24;
 
-    std::vector<int> result = twoSumBruteForce(nums, target);
+    std::pair<int, int> bruteResult =
+        twoSumBruteForce(nums, target);
 
-    std::cout << "Brute-force indices: "
-              << result[0] << ", " << result[1] << std::endl;
+    std::pair<int, int> hashResult =
+        twoSumHash(nums, target);
+
+    std::cout << "Brute-force indices: ["
+          << bruteResult.first << ", "
+          << bruteResult.second << "]" << std::endl;
+
+    std::cout << "Brute-force values: ["
+          << nums[bruteResult.first] << ", "
+          << nums[bruteResult.second] << "]" << std::endl;
+
+    std::cout << "Hash-table indices: ["
+          << hashResult.first << ", "
+          << hashResult.second << "]" << std::endl;
+
+    std::cout << "Hash-table values: ["
+          << nums[hashResult.first] << ", "
+          << nums[hashResult.second] << "]" << std::endl;
 
     return 0;
 }
